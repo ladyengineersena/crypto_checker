91bdbbe65ba9ec5d821f1105d492aaf153dacf09
Sena <129009212+ladyengineersena@users.noreply.github.com>
2024-11-24 20:12:39 +0300

    Add files via upload

---
+++ crypto_checker.py
@@ -0,0 +1,34 @@
+from cryptocompare import get_price
+
+btc = get_price("BTC", currency='USDT')
+print(btc)
+
+eth = get_price("ETH", currency='USDT')
+print(eth)
+
+pups = get_price("PUPS", currency='USDT')
+print(pups)
+
+doge = get_price("DOGE", currency='USDT')
+print(doge)
+
+sol = get_price("SOL", currency='USDT')
+print(sol)
+
+bnb = get_price("BNB", currency='USDT')
+print(bnb)
+
+xrp = get_price("XRP", currency='USDT')
+print(xrp)
+
+ada = get_price("ADA", currency='USDT')
+print(ada)
+
+usdc = get_price("USDC", currency='USDT')
+print(usdc)
+
+trx = get_price("TRX", currency='USDT')
+print(trx)
+
+ton = get_price("TON", currency='USDT')
+print(ton)
